f4bb0f8e61611b67e3212ec7b07a56aac635135c
ron93 <ronmkare1@gmail.com>
2021-02-17 00:47:23 +0300

    login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,20 @@
 from app.auth import auth_bp 
 from flask import render_template,flash, redirect, url_for
 from app.auth.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated():
+        return redirect(url_for('main_bp.index'))
     form = LoginForm()
     
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+        login_user(user,remember=form.remember_me.data)
         return redirect(url_for('main_bp.index'))
     return render_template('auth/login.html',title='Sign in',form=form)
 
