3ed3cfeb770ca5c28c521676e0757c7ee69f0bdd
ron93 <ronmkare1@gmail.com>
2021-02-16 22:32:42 +0300

    Login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,9 +1,11 @@
 from app.auth import auth_bp
-
+from flask import render_template
+from app.auth.forms import LoginForm
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    return render_template('auth/login.html',title='Sign in',form=form)
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
