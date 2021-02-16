8a33b6a8f0a4ec839603ca1a0a0fe9349aee86e6
ron93 <ronmkare1@gmail.com>
2021-02-16 22:47:08 +0300

    Login -- accept input, flash errors

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,15 @@
 from app.auth import auth_bp
-from flask import render_template
+from flask import render_template,flash, redirect
 from app.auth.forms import LoginForm
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
     return render_template('auth/login.html',title='Sign in',form=form)
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
