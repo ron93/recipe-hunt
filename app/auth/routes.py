63df9b8ac1136bf3fc485d63ecfae65b44404692
ron93 <ronmkare1@gmail.com>
2021-02-17 12:38:39 +0300

    registration route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,7 @@
+from app import db
 from app.auth import auth_bp 
 from flask import render_template,flash, redirect, url_for,request
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 from werkzeug.urls import url_parse
 
@@ -33,5 +34,15 @@ def logout():
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    if current_user.is_authenticated:
+        return redirect(url_for('main_bp.index'))
 
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("new user registered!")
+        return redirect(url_for('auth_bp.login'))
+    return render_template('auth/register.html', title='Register', form=form)
