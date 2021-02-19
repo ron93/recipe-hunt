32d198fb2165202cc38c48106bcba76c6204c853
ron93 <ronmkare1@gmail.com>
2021-02-19 03:09:42 +0300

    last seen and about

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+from app import db
 from app.main import main_bp 
 from flask import render_template
 from flask_login import login_required,current_user
 from app.models import User
+
+@main_bp.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @main_bp.route('/', methods=['GET'])
