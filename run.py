b22cfd4ae0948d5f1c6cdeeaede70244b3783978
ron93 <ronmkare1@gmail.com>
2021-03-01 21:52:34 +0300

    proj base

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-from app import create_app
+from app import create_app, db, cli
+from app.models import User, Recipe, Message, Notification, Task
 
 app = create_app()
+cli.register(app)
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db}
-    
-if __name__ == '__main__':
-    app.run(debug=True)
+    return {'db': db, 'User': User, 'Recipe': Recipe, 'Message': Message,
+            'Notification': Notification, 'Task': Task}
